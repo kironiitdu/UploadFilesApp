11053cbb8be181a06720f32802db5dac2df9d0fb
Md Farid Uddin <v-mdud@microsoft.com>
2023-05-12 13:11:31 +0800

    Updated

--- UploadFilesApp/Models/ReponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UploadFilesApp.Models
-{
-    public class ReponseModel
-    {
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-        public bool IsResponse { get; set; }
-    }
-}
